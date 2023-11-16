04daada1e846d8d6c38a0fe9969d1843f37130c7
FarizRahmanM <fariz.rahman.tif421@polban.ac.id>
2023-11-16 11:33:11 +0700

    backend regis dan sign up

--- app.py
+++ app.py
@@ -1,32 +1,71 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, redirect, url_for, flash, session, request
+# ... (other imports)
+
+from flask_pymongo import PyMongo
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email
 
 app = Flask(__name__)
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/Tubes'
+app.secret_key = 'bond123'
+mongo = PyMongo(app)
 
-@app.route("/")
-def home():
-    return "Welcome to your Flask application!"
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Register')
 
-@app.route("/login", methods=["GET", "POST"])
+@app.route('/')
+def index():
+    form = RegistrationForm()
+    return render_template('register.html', form=form)
+
+@app.route('/register', methods=['POST'])
+def signup():
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        users = mongo.db.users
+        existing_user = users.find_one({'username': form.username.data})
+
+        if existing_user is None:
+            users.insert_one({
+                'username': form.username.data,
+                'password': form.password.data,
+                'email': form.email.data
+            })
+            flash('Registration successful!', 'success')
+            return redirect(url_for('index'))
+
+        flash('Username already exists. Choose a different one.', 'danger')
+
+    return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == "POST":
-        # Process the login form data here
-        # You can access form data like this: request.form['userid'] and request.form['passw']
-        # Implement your login logic here
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
 
-        return "Login successful!"  # You can redirect or display a success message as needed
+        users = mongo.db.users
+        existing_user = users.find_one({'username': username})
 
-    return render_template("login.html")
+        if existing_user and existing_user['password'] == password:
+            session['username'] = username
+            flash(f'Hello, {username}! You are now logged in.', 'success')
+            return render_template('login.html')
 
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    if request.method == "POST":
-        # Process the login form data here
-        # You can access form data like this: request.form['userid'] and request.form['passw']
-        # Implement your login logic here
+        flash('Invalid username or password. Please try again.', 'danger')
 
-        return "Register successful!"  # You can redirect or display a success message as needed
+    return render_template('login.html')
 
-    return render_template("register.html")
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    flash('You have been logged out.', 'info')
+    return redirect(url_for('index'))
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
